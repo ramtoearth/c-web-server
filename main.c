ecb670f1149e4639c2f52e271d52f7292d9df3a9
Ramses Mata <rmatah@amazon.com>
2026-08-15 00:20:53 -0600

    Add comments and notes

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 int main () {
   // socket ===================================
+  // man 2 socket
   int sck = socket(AF_INET, SOCK_STREAM, 0);
   if (sck < 0) {
     perror("socket");
@@ -13,19 +14,21 @@ int main () {
   }
 
   struct sockaddr_in addr;
-  memset(&addr, 0, sizeof(addr));
+  memset(&addr, 0, sizeof(addr)); // inicia este segmento de memoria en 0s (no es lo mismo para todos los tipos de datos)
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(8080);
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(8080); // little-endian -> big-endian | h: host, to, n: network, s: short
+  addr.sin_addr.s_addr = htonl(INADDR_ANY); // h: host, to, n: network, l: long
 
   // bind ===================================
-  int bind_r = bind(sck, (struct sockaddr *) &addr, sizeof(addr));
+  // man 2 bind
+  int bind_r = bind(sck, (struct sockaddr *) &addr, sizeof(addr)); // El casteo en C funcina como (tipo)valor
   if (bind_r < 0) {
     perror("bind");
     return EXIT_FAILURE;
   }
 
   // listen ===================================
+  // man 2 listen
   int listen_r = listen(sck, 16);
   if (listen_r < 0) {
     perror("listen");
